bc3203531360154f4670aac77bfb855e89195465
Peter Bestoso <Peter Bestoso>
2017-03-03 14:20:14 -0500

    basic flask app

--- server.py
+++ server.py
@@ -1,30 +1,33 @@
+import os
+
 from flask import Flask, jsonify, request, abort 
 
-LIGHT_IDS = [
- 
+LIGHT_IDS = [7, 6, 5, 4, 1]
+NUM_LIGHTS = 5
+    
 app = Flask(__name__) 
 
-@app.route('/oscarlight/v1/<int:light_id>', methods=['PUT'])
+def set_light(light_id, brightness):
+    os.system("echo " + LIGHT_IDS[light_id] + "=" + brightness
+              + " > /dev/servoblaster")
+    
+@app.route('/', methods=['PUT'])
 def put(light_id):
-    if light_id < 0 or light_id >= NLIGHTS:
-        print("bad light number")
-        abort(404 , "light id was bad" )
+    light_id = request.args.get('light_id')
+    brightness = request.args.get('brightness')
+     
+    if not light_id or light_id < 0 or light_id >= 5:
+        abort(400)
 
-    if not request.json:
-        print("not json ")
-        abort(400 , "not json" )
+    if not brightness or brightness < 0 or brightness >= 900:
+        abort(400)
 
-    print( "now trying") 
-
-    on_off = request.json.get('switch')
-
-    print( on_off ) 
-
-    turn( light_id, on_off )
+    set_light(light_id, brightness)
+        
     return '{}'
 
 if __name__ == '__main__':
     for i in range(0, NLIGHTS):
-        turn(i, ON)
+        set_light(i, 0)
  
     app.run(debug=True)
