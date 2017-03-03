ff42acc408a609d26707294bf44ea3fb99b67270
Sam Gross <sam@samthegeek.net>
2017-03-03 13:32:24 -0500

    temp

--- steve.sh
+++ steve.sh
@@ -1,7 +1,10 @@
+#! /bin/bash
+
 start=10
+max=800
 c=$start
 inc=2
-while [[ $c -lt 800 ]] ; do 
+while [[ $c -lt $max ]] ; do 
 	echo -e "7=$c\n6=$c\n5=$c\n4=$c\n1=$c" > /dev/servoblaster; 
 	c=$(($c+$inc)); 
 	sleep 0.001
