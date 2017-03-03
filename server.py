6bb91090fa17dae63bc899641b72b5b681152582
Peter Bestoso <Peter Bestoso>
2017-03-03 18:14:27 -0500

    not so dark

--- server.py
+++ server.py
@@ -60,7 +60,7 @@ def wave_lights():
             if count_downs[i] == 0:
                 if not turning_up[i]:
                     set_light(i, LIGHT_BRIGHTS[i] - .005)
-                    if (LIGHT_BRIGHTS[i] <= 0):
+                    if (LIGHT_BRIGHTS[i] <= .1):
                         turning_up[i] = True
                 else:
                     if (LIGHT_BRIGHTS[i] < MAX_BRIGHTNESS):
