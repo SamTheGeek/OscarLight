2f4e5c003f1d0fc652727da44b40b0b6ec81e2d9
Raul Santos <raul@hioscar.com>
2017-04-26 10:08:39 -0400

    Fixed Url make production

---
+++ www/indexold.php
@@ -0,0 +1,16 @@
+<title> Oscar Lights! </title>
+<html>
+Oscar Lights!
+<br> <br>
+<a href="/up" onclick="javascript:event.target.port=5000">on</a>
+<br> <br>
+<a href="/down" onclick="javascript:event.target.port=5000">fade off</a>
+<br> <br>
+<a href="/blink" onclick="javascript:event.target.port=5000">blink</a>
+<br> <br>
+<a href="/wave" onclick="javascript:event.target.port=5000">wave</a>
+<br> <br>
+<a href="/sparkle" onclick="javascript:event.target.port=5000">sparkle</a>
+<br>
+<br>
+</html>
